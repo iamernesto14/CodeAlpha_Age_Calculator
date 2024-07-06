7f6ae2e400db4599f5a09d69eac0d30ea17b0ba4
ERNEST ANOKYE <44618031+iamernesto14@users.noreply.github.com>
2024-07-06 23:17:20 +0000

    mobile view completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      // custom font
+      fontFamily: {
+        "poppins": ['Poppins', 'sans-serif'],
+      },
+      // Custom colors
+      colors: {
+        'primary-puple': '#854dff',
+        'primary-light-red': '#ff5757',
+        'neutral-off-black': '#141414',
+        'neutral-grey': {
+          100: '#dbdbdb',
+          200: '#716f6f'
+        },
+        'neutral-white': {
+            100: '#ffffff',
+            200: '#f0f0f0'
+        }
+      },
+      italic: {
+        fontStyle: 'italic',
+      },
+    },
   },
   plugins: [],
 }
