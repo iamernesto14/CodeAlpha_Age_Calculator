90fefffdcc45a920f5dd74276d03160f04b30e80
ERNEST ANOKYE <44618031+iamernesto14@users.noreply.github.com>
2024-07-19 22:14:31 +0000

    task complete

--- src/script.js
+++ src/script.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const dayInput = document.getElementById('day');
   const monthInput = document.getElementById('month');
   const yearInput = document.getElementById('year');
-  const displayDay = document.getElementById('display_day');
-  const displayMonth = document.getElementById('display_month');
-  const displayYear = document.getElementById('display_year');
+  const display = document.querySelectorAll
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', calculateAge);
 
-  
+  function calculateAge() {
+
+  }
+
+})
