fde42358d18da66ec72f36113639925804e6fef1
ERNEST ANOKYE <44618031+iamernesto14@users.noreply.github.com>
2024-07-19 22:58:16 +0000

    task completed

--- src/script.js
+++ src/script.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+  
+  // Get the input fields
   const dayInput = document.getElementById('day');
   const monthInput = document.getElementById('month');
   const yearInput = document.getElementById('year');
-  const display = document.querySelectorAll
+
+  // Get the button tag
   const btn = document.getElementById('btn');
 
-  btn.addEventListener('click', calculateAge);
+// Add an event handler to the day input
+dayInput.addEventListener('input', function () {
+  // checkng for error in the input feld
+  if (dayInput.nextElementSibling.innerHTML === 'Must be valid date') {
+    dayInput.classList.remove('error');
+    monthInput.classList.remove('error');
+    yearInput.classList.remove('error');
+  }
+})
 
   function calculateAge() {
 
