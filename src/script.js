3d567308c778e2ff69e33bce31cc23ab89082307
ERNEST ANOKYE <44618031+iamernesto14@users.noreply.github.com>
2024-07-20 23:13:07 +0000

    task complete

--- src/script.js
+++ src/script.js
@@ -12,11 +12,60 @@ document.addEventListener('DOMContentLoaded', () => {
 dayInput.addEventListener('input', function () {
   // checkng for error in the input feld
   if (dayInput.nextElementSibling.innerHTML === 'Must be valid date') {
-    dayInput.classList.remove('error');
-    monthInput.classList.remove('error');
-    yearInput.classList.remove('error');
+    dayInput.parentElement.classList.remove('error');
+    monthInput.parentElement.classList.remove('error');
+    yearInput.parentElement.classList.remove('error');
   }
-})
+
+  // check if date input is greater than 31, then we add the error class
+  if (dayInput.value > 31) {
+    dayInput.nextElementSibling.innerHTML = "Must be valid day";
+    dayInput.classList.add('error');
+    dayInput.classList.remove('focus:outline-primary-puple');
+  } else {
+    dayInput.classList.remove('error')
+    dayInput.classList.add('focus:outline-primary-puple')
+  }
+});
+
+// Add event handler to the month input
+monthInput.addEventListener('input', function () {
+  // checking for error in the month input field
+  if (monthInput.nextElementSibling.innerHTML = "Must be a valid date") {
+    dayInput.parentElement.classList.remove('error');
+    monthInput.parentElement.classList.remove('error');
+    yearInput.parentElement.classList.remove('error');
+  }
+
+  // check if a month is greater than 12, we signal an error
+  if (monthInput.value > 12) {
+    monthInput.nextElementSibling.innerHTML = 'Must be a valid month';
+    monthInput.parentElement.classList.add('error');
+  } else {
+    monthInput.parentElement.classList.remove('error')
+  }
+});
+
+// Ad event handler to the year input
+yearInput.addEventListener('input', function () {
+  // checking for error in year input field
+  if (yearInput.nextElementSibling.innerHTML = "Must be a valid date") {
+    dayInput.parentElement.classList.remove('error');
+    monthInput.parentElement.classList.remove('error');
+    yearInput.parentElement.classList.remove('error');
+  }
+
+  // checking if the specified year is greater than current year
+  let Data = new Date();
+  if (yearInput.value > Data.getFullYear()) {
+    yearInput.nextElementSibling.innerHTML = "Must be in the past";
+    yearInput.parentElement.classList.add('error'); 
+  } else {
+    yearInput.parentElement.classList.remove('error');
+  }
+});
+
+
 
   function calculateAge() {
 
