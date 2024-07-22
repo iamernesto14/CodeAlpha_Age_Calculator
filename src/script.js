f9ceaa5009aa56d266c2a4dd00b035eb53b75c8b
ERNEST ANOKYE <44618031+iamernesto14@users.noreply.github.com>
2024-07-22 19:17:19 +0000

    task completed

--- src/script.js
+++ src/script.js
@@ -24,5 +24,42 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!day || !month || !year) {
       return;
     }
+
+    let date = new Date(year, month - 1, 0)
+    let currentData = new Date();
+    
+    if (!(date.getFullYear() == year && date.getMonth() == month - 1 && date.getDate() == day) || (document.getElementsByClassName('error').length) || date > currentData || year < 0) {
+      alert('Must be a valid date');
+    }
+
+    let age_year = currentData.getFullYear() - date.getFullYear();
+    let age_month = 0;
+    let age_day = 0;
+
+    if (currentData < new Date(currentData.getFullYear(), month - 1, day)) {
+      age_year = age_year - 1;
+      age_month = currentData.getMonth() + 1;
+      age_day = currentData.getDate();
+    } else {
+      if (currentData.getMonth() + 1 === month) {
+        age_mounth = 0;
+        age_day = currentData.getDate() - day;
+        console.log(age_day);
+      } else {
+        age_mounth = currentData.getMonth() + 1 - month;
+        if (currentData.getDate() < day) {
+          age_month = age_month - 1;
+          age_day = currentData.getDate() + new Date(currentData.getFullYear(), currentData.getMonth(), 0).getDate() - day;
+        } else {
+          age_day = currentData.getDate() - day;
+        }
+      }
+    }
+
+    const displayYear = document.getElementById('display_year').textContent = age_year;
+    const displayMonth = document.getElementById('display_month').textContent = age_month;
+    const displayDay = document.getElementById('display_day').textContent = age_day;
+
+     
   })
 });
